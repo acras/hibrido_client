cab88df15243867f5e772890eb20c1cd74063593
Ricardo Acras <ricardo@acras.com.br>
2013-04-02 11:29:40 -0300

    comentários no IDataPrincipal

--- lib/IDataPrincipalUnit.pas
+++ lib/IDataPrincipalUnit.pas
@@ -7,15 +7,29 @@ uses IBQuery;
 type
 
 IDataPrincipal = interface
-  procedure startTransaction; overload;
-  procedure startTransaction(name: string); overload;
-  procedure commit; overload;
-  procedure commit(name: string); overload;
-  function getSQLResult(sqlText: string): variant;
-  function getSQLIntegerResult(sqlText: string): integer;
-  procedure execSQL(sqlText: string);
-  procedure refreshData;
-  function getQuery: TIBQuery;
+  procedure startTransaction; overload;                     //Inicia uma transação no banco de dados
+
+  procedure startTransaction(name: string); overload;       //Inicia uma transação nomeada no banco
+                                                            //de dados. Geralmente pode chamar a
+                                                            //não nomeada simplesmente.
+
+  procedure commit; overload;                               //faz commit dos dados da transação
+                                                            //genérica
+
+  procedure commit(name: string); overload;                 //faz o commit de transação nomeada
+
+  function getSQLResult(sqlText: string): variant;          //retorna o resultado de uma query
+                                                            //ele pode ser de qualquer tipo
+
+  function getSQLIntegerResult(sqlText: string): integer;   //retorna um resultado inteiro
+                                                            //de uma query. Útil em queries de count e max
+
+  procedure execSQL(sqlText: string);                       //executa um comando SQL sem resultado
+
+  procedure refreshData;                                    //faz refresh dos dados do client
+                                                            //em firebird por exemplo é um Commit Work
+
+  function getQuery: TIBQuery;                              //retorna um TIBquery conectado na base
 end;
 
 implementation
